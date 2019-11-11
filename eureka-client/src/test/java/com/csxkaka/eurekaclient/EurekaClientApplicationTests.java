e9719832da9a30ddbc9a4067152d4b7e3d5497b0
csxkaka <970972786@qq,com>
2019-11-11 21:18:07 +0800

    SpringBoot 学习

---
+++ eureka-client/src/test/java/com/csxkaka/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,9 @@
+package com.csxkaka.eurekaclient;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+
+}
---
+++ eureka-server/src/test/java/com/csxkaka/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,8 @@
+package com.csxkaka.eurekaserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+}
